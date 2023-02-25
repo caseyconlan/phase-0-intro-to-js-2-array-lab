8c4b012f751b9780903195876e00dda0e13bfcfa
Casey Conlan <caseyconlanshopping@gmail.com>
2023-02-25 10:26:04 -0800

    9 passed tests on Arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+    return cats
+   }
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+    return cats
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop("Ralph")
+    return cats
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift("Bob")
+    return cats
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push("Broom")
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift("Arnold")
+    return copyOfCats
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop("Broom")
+    return copyOfCats
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift("Arnold")
+    return copyOfCats
+}
